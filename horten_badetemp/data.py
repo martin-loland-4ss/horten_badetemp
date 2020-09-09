cb2454462d510ca84ae0d3ff9165f30bce1a41bb
Martin Løland <martinloland@gmail.com>
2020-09-09 20:16:08 +0200

    new func

--- horten_badetemp/data.py
+++ horten_badetemp/data.py
@@ -2,8 +2,14 @@ import requests
 
 
 def get_data(api_url):
-    "get text from website (api_url) and return content (str)"
+    "get text from api_url (str) and return content (str)"
 
 
 def extract_latlon(data):
     "extract latitudes and longitudes from data (dict) and return (tuple of two lists with numbers)"
+
+
+def raw_data_to_dict_list(data):
+    """convert data (dict) to (list of dict) 
+    where each item in list is feature with 'location', 'temperature' and 'updated'
+    """
