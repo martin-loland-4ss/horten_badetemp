85cf7869401af719d7b023829234c57943646e16
Martin Løland <martinloland@gmail.com>
2020-09-09 20:03:44 +0200

    file read

--- horten_badetemp/readers.py
+++ horten_badetemp/readers.py
@@ -2,6 +2,8 @@
 
 def file_reader(filepath):
     "read content of filepath (str) and return content (str)"
+    with open(filepath, 'r') as f:
+        return f.read()
 
 
 def json2dict(json_text):
