0690382d743f459634f516373503a1473e353846
Martin Løland <martinloland@gmail.com>
2020-09-09 20:48:24 +0200

    funcs

--- horten_badetemp/map.py
+++ horten_badetemp/map.py
@@ -27,13 +27,26 @@ def get_markers(data):
     """use data (list of dict with keys: 'location', 'coordinate', 'temperature' and 'updated')
     return (list of ipyleaflet.Marker objects), use get_marker and popup_html function above
     """
+    markers = []
+    for feature in data:
+        html = popup_html(
+            location=feature["location"],
+            temperature=feature["temperature"],
+            updated=feature["updated"]
+        )
+        marker = get_marker(coordinate=feature["coordinate"], popup_html=html)
+        markers.append(marker)
 
 
 def add_markers_to_map(map, markers):
     """use map (ipyleaflet.Map object) and markers (list of ipyleaflet.Marker objects)
     add markers to map and return (ipyleaflet.Map object)
     """
+    for marker in markers:
+        map.add_layer(marker)
+    return map
 
 
 def add_fullscreen_control(map):
     "use map (ipyleaflet.Map object), add fullscreen control and return object"
+    map.add_control(ipyleaflet.FullScreenControl())
