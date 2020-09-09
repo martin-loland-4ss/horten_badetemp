ceb2fb04686e2a249f079f3619b5659dd88632e5
Martin Løland <martin.loland@4subsea.com>
2020-09-09 16:49:09 +0200

    more functions

--- horten_badetemp/transformers.py
+++ horten_badetemp/transformers.py
@@ -2,6 +2,13 @@
 def average_latlon(latitudes, longitudes):
     "take in latitudes and longitudes (list of numbers) and return averages (tuple of two numbers)"
 
-def timestamp2human()
 
-def 
+def timestamp2human(timestamp):
+    "read timestamp (str) and return human x days/hours/minutes ago (str)"
+
+
+def geojson_to_datalist(geojson):
+    """use geojson (dict)
+    return (list of dict with keys 'location', 'temperature' and 'updated')
+    use timestamp2human function above to convert timestamp
+    """
