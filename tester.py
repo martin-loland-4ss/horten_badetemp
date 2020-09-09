9caab07d413be94df86f703b60124df723bcd158
Martin Løland <martinloland@gmail.com>
2020-09-09 19:59:44 +0200

    removed tester

--- tester.py
+++
@@ -1,8 +0,0 @@
-from horten_badetemp.data import *
-from horten_badetemp.map import *
-from horten_badetemp.readers import *
-from horten_badetemp.transformers import *
-
-num = 4
-result = squared(num)
-print(f"the squared of {num} is {result}")
