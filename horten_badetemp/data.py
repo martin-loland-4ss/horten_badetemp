13e37665f0595401278616415d735a67a66b54fc
Martin Løland <martinloland@gmail.com>
2020-09-09 20:21:07 +0200

    raw data

--- horten_badetemp/data.py
+++ horten_badetemp/data.py
@@ -15,3 +15,12 @@ def raw_data_to_dict_list(data):
     """convert data (dict) to (list of dict) 
     where each item in list is feature with 'location', 'temperature' and 'updated'
     """
+    array = []
+    for feature in data["features"]:
+        d = {
+            "location": feature["geometry"]['coordinates'],
+            "temperature": feature["properties"]['last'],
+            "updated": feature["properties"]['time']
+        }
+        array.append(d)
+    return array
