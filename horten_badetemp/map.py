e297ef4776f874ad2bf22229bbc803ed4fc52a2d
Martin Løland <martinloland@gmail.com>
2020-09-09 20:43:13 +0200

    change

--- horten_badetemp/map.py
+++ horten_badetemp/map.py
@@ -10,13 +10,13 @@ def popup_html(location, temperature, updated):
     "use location (str), temperature (str) and updated (str), return ipywidgets.HTML object"
 
 
-def get_marker(location, popup_html):
-    "use location (tuple of two numbers) and popup_html (ipywidgets.HTML), return ipyleaflet.Marker object"
+def get_marker(coordinate, popup_html):
+    "use coordinate (tuple of two numbers) and popup_html (ipywidgets.HTML), return ipyleaflet.Marker object"
 
 
 def get_markers(data):
-    """use data (list of dict with keys: 'location', 'temperature' and 'updated')
-    return (list of ipyleaflet.Marker objects), use get_marker function above
+    """use data (list of dict with keys: 'location', 'coordinate', 'temperature' and 'updated')
+    return (list of ipyleaflet.Marker objects), use get_marker and popup_html function above
     """
 
 
