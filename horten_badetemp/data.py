0bc67ba2d2ead4ef80a58ff4cc77c153d064332d
Martin Løland <martinloland@gmail.com>
2020-09-09 20:08:59 +0200

    get data

--- horten_badetemp/data.py
+++ horten_badetemp/data.py
@@ -3,6 +3,8 @@ import requests
 
 def get_data(api_url):
     "get text from website (api_url) and return content (str)"
+    r = requests.get(api_url)
+    return r.text()
 
 
 def extract_latlon(data):
