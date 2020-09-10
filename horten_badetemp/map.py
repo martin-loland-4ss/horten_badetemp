18ffad98fc8c94d254c6f097d95a91b9ee9ab765
thomas-opheim-4ss <71003760+thomas-opheim-4ss@users.noreply.github.com>
2020-09-10 14:10:08 +0200

    Update map.py
    Forslag til løsning

--- horten_badetemp/map.py
+++ horten_badetemp/map.py
@@ -30,7 +30,8 @@ def add_markers_to_map(map, markers):
 
 def add_fullscreen_control(map):
     "use map (ipyleaflet.Map object), add fullscreen control and return object"
-
+    ob = add_control(FullScreenControl())
+    return ob 
 
 def get_master_map():
     """Use the following functions to generate a map:
