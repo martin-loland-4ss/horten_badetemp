e7edf1e9214d527a08a8681add509a32f0cf0b82
Håvard Skjerve <hsk@4subsea.com>
2020-09-10 14:37:55 +0200

    Extract  latlon (#33)
    * Update data.py
    
    Added code to extract_latlon
    
    * Update data.py
    
    Restructured code

--- horten_badetemp/data.py
+++ horten_badetemp/data.py
@@ -9,7 +9,14 @@ def get_data(api_url):
 
 def extract_latlon(data):
     "extract latitudes and longitudes from data (list of dict) and return (tuple of two lists with numbers)"
+    
+    lat, lon = [], []
 
+    for loc in data:
+        lat.append(loc['coordinate'][0])
+        lon.append(loc['coordinate'][1])
+    
+    return lat, lon 
 
 def raw_data_to_dict_list(data):
     """convert data (dict) to (list of dict) 
