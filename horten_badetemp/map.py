aabd2d439177c8ffd7ec58903e0101b07596f023
vetle-kjarstad-4ss <70746867+vetle-kjarstad-4ss@users.noreply.github.com>
2020-09-10 14:50:51 +0200

    Update map.py (#24)
    * Update map.py
    
    including functions
    
    * Update map.py
    
    updated zoom

--- horten_badetemp/map.py
+++ horten_badetemp/map.py
@@ -57,3 +57,11 @@ def get_master_map():
         add_fullscreen_control
         return map (ipyleaflet.Map object)
     """
+    features = get_features()
+    lat, lon = extract_latlon(features)
+    center = avg_latlon(latitudes=lat, longitudes=lon)
+    map = get_map(center=center, zoom=11)
+    mrk = get_markers(features)
+    map = add_markers_to_map(map=map, markers=mrk)
+    map = add_fullscreen_control(map)
+    return map
